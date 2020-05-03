56b5543a2b137f01f577be717ac3fbba51749e43
yevheniikoliesnik <yevhenii.ko@ideo-digital.com>
2020-05-03 17:27:32 +0300

    first init commit

---
+++ src/components/TodoInput.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TodoInput extends Component {
+    render() {
+        return (
+            <h1>
+                hellow from todo input
+            </h1>
+        )
+    }
+}
